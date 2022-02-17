5fa75eb471bc9431ddf29a6c7a88133994a72c91
jayphen <jayphenyt@gmail.com>
2022-02-18 00:30:28 +1000

    uwu tain daddy

--- site/index.js
+++ site/index.js
@@ -1,5 +1,18 @@
 cumCounter = 0;
 
+document.querySelectorAll('.gaybtn').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        alert('Yes. 100%');
+    });
+});
+
+document.querySelectorAll('.straightbtn').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        alert('no.');
+    });
+});
 
 document.querySelectorAll('.modern-btn').forEach(function(btn) {
     btn.addEventListener('click', function(e) {
